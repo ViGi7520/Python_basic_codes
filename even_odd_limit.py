c28b8f5fb4b7afe4d3fa71c08ca9aec7135f7d8f
ViGi <vighnesh7520@gmail.com>
2021-07-26 13:28:23 +0530

    Inital commit

--- even_odd_limit.py
+++ even_odd_limit.py
@@ -1,10 +1,11 @@
-def showNumbers(limit):
-    for i in range(0,limit+1):
-        if i%2==0:
+def shownumbers(start, end):
+    for i in range(start, end+1):
+        if i % 2 == 0:
             print(i, "Even")
-        else :
+        else:
             print(i, "Odd")
 
 
-(x) = int(input("enter :"))
-print(showNumbers(x))
+start = int(input('Enter the Start : '))
+end = int(input('Enter the End : '))
+print(shownumbers(start, end))
