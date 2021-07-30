67823180483a46845c5116c8dc4ad644c4a8a5ac
ViGi7520 <84280996+ViGi7520@users.noreply.github.com>
2021-07-30 14:52:48 +0530

    Add files via upload

---
+++ Odd & Even/odd&even_range.py
@@ -0,0 +1,8 @@
+#odd even number using range function
+#range(start,end,step)
+
+print("even numbers till 20:")
+print(list(range(2, 21,2)))
+
+print("odd numbers till 20:")
+print(list(range(1, 20,2)))
